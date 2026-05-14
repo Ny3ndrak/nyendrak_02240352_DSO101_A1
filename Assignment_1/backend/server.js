deb2f0ec19ec6c8bbbccf2481b771689a3e08104
Ny3ndrak <nyendrakz@gmail.com>
2026-05-14 07:26:26 +0600

    server.js

--- Assignment_1/backend/server.js
+++ Assignment_1/backend/server.js
@@ -5,7 +5,10 @@ const cors = require("cors");
 const { initDatabase, getPool } = require("./db");
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
 
 app.use(cors());
 app.use(express.json());
